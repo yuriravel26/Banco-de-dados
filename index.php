dfd271f45c31bed6e5a7e2123764c3f2cd11f0e0
Alice <alice.holanda.136@ufrn.edu.br>
2022-05-12 11:23:09 -0300

    2 php

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BD web 4</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.html">Sobre fdsfsdfsd</a>
+                </li>
+                <li>
+                    <a href="project.html">Projeto</a>
+                </li>
+                <li>
+                    <a href="contact.html">Contato</a>
+                </li>
+            </ul>
+        </nav>
+    
+        <header>
+            <ul>
+                <div class="start">
+                    <img src="C:\Users\yuri_\OneDrive\Documentos\easy-portfolio\png\Lau.png"></img>
+                    <p>Antonio Laurentino</p>
+                </div>
+                <div class="center">
+                    <img src="C:\Users\yuri_\OneDrive\Documentos\easy-portfolio\png\Alice.png"></img>
+                    <p>Alice Bastos</p>
+                </div>
+                <div class="end">
+                    <img src="C:\Users\yuri_\OneDrive\Documentos\easy-portfolio\png\Yuri.png"></img>
+                    <p>Yuri Ravel</p>
+                </div>
+            </ul>
+        </header>
+
+        <section class="Yuri">
+            <h1>Yuri Ravel</h1>
+            <h2>Estudante de C&T</h2>
+            <h3>Sobre mim</h3>
+                <p>
+                    Me chamo Yuri Ravel, tenho 20 anos e moro na cidade de Natal/RN, 
+                    sou estudante de ciencias e tecnologias na Universidade Federal do Rio Grande do Norte. 
+                    Gosto muito de jogar no computador, principalmente jogos de FPS e estou a treinar sobre programação para minha
+                    carreira academica.
+                </p>
+            </section>
+        
+        <section class="Alice">
+            <h1 id="alice">Alice Bastos</h1>
+            <h2>Estudante de C&T</h2>
+            <h3>Sobre mim</h3>
+                <p>
+                    Me chamo Alice Bastos, tenho 29 anos e moro na cidade de Natal/RN, 
+                    sou estudante de ciencias e tecnologias na Universidade Federal do Rio Grande do Norte. 
+                    
+                </p>
+            </section>
+        </main>
+    </div>
+
+    
+</body>
+</html>
