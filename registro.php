a9e4a893e582fe5b3715f354b1fff0920c03c5a9
Yuri <yuri_ravel20@hotmail.com>
2022-05-26 11:17:27 -0300

    registro mudado

--- registro.php
+++ registro.php
@@ -4,59 +4,115 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulário | GN</title>
     <style>
         body{
             font-family: Arial, Helvetica, sans-serif;
             background-image: linear-gradient(45deg, cyan, yellow);
         }
-        .registro{
+        .box{
             background-color: rgba(0, 0, 0, 0.9);
             position: absolute;
             top: 50%;
             left: 50%;
             transform: translate(-50%,-50%);
-            padding: 50px;
+            padding: 20px;
             border-radius: 15px;
             color: white;
         }
-        input{
-            padding: 15px;
+        fieldset{
+            border: 3px solid dodgerblue;
+        }
+        legend{
+            border: 1px solid dodgerblue;
+            padding: 10px;
+            text-align: center;
+            background-color: dodgerblue;
+            border-radius: 8px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
             border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: dodgerblue;
+        }
+        #data_nascimento{
+            border: none;
+            padding: 8px;
+            border-radius: 10px;
             outline: none;
             font-size: 15px;
         }
-        button{
-            background-color: dodgerblue;
+        #submit{
+            background-image: linear-gradient(to right,rgb(0, 92, 197), rgb(90, 20, 220));
+            width: 100%;
             border: none;
             padding: 15px;
-            width: 100%;
-            border-radius: 10px;
             color: white;
             font-size: 15px;
             cursor: pointer;
+            border-radius: 10px;
         }
-        button:hover{
-            background-color: deepskyblue;
-        }
-        a{
-            color: white;
-            text-align: center;
+        #submit:hover{
+            background-image: linear-gradient(to right,rgb(0, 80, 172), rgb(80, 19, 195));
         }
     </style>
 </head>
 <body>
-    <div class="registro">
-        <h1>Registro</h1>
-        <input type="text" placeholder="Nome">
-        <br><br>
-        <input type="text" placeholder="Sobrenome">
-        <br><br>
-        <input type="text" placeholder="Email">
-        <br><br>
-        <input type="password" placeholder="Senha">
-        <br><br>
-        <button>Enviar</button>
+    <div class="box">
+        <form action="">
+                <h1>Registro</h1>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">Nome completo</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">Email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="telefone" class="labelInput">Telefone</label>
+                </div>
+                <p>Sexo:</p>
+                <input type="radio" id="feminino" name="genero" value="feminino" required>
+                <label for="feminino">Feminino</label>
+                <br>
+                <input type="radio" id="masculino" name="genero" value="masculino" required>
+                <label for="masculino">Masculino</label>
+                <br>
+                <input type="radio" id="outro" name="genero" value="outro" required>
+                <label for="outro">Outro</label>
+                <br><br>
+                <label for="data_nascimento"><b>Data de Nascimento:</b></label>
+                <input type="date" name="data_nascimento" id="data_nascimento" required>
+                <br><br>
+                <input type="submit" name="submit" id="submit">
+            </fieldset>
+        </form>
     </div>
 </body>
 </html>
