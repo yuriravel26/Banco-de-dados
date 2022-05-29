53582615269f74b572ebedf0b9f2ae5c9e13c98e
Yuri <yuri_ravel20@hotmail.com>
2022-05-29 13:00:10 -0300

    conexao

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "bdweb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conexao = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
