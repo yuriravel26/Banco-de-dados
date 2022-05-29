b11521dd692c13ab20424eb1a0bed8887416880c
Yuri <yuri_ravel20@hotmail.com>
2022-05-29 13:21:27 -0300

    conexao mudada

--- conexao.php
+++ conexao.php
@@ -4,23 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "bdweb";
 
-// Create connection
+
 $conexao = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  echo "Erro";
 }
-
-$sql = "SELECT id, email FROM usuario";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Email: " . $row["email"]."<br>";
-  }
-} else {
-  echo "0 results";
+else{
+  echo "Conexão efetuada com sucesso";
 }
-$conn->close();
-?>
