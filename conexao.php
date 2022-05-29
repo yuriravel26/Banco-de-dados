efaa9d7606cdce19bd4fd137b8ab84defb83bb30
Yuri <yuri_ravel20@hotmail.com>
2022-05-29 13:24:25 -0300

    desfazer

--- conexao.php
+++ conexao.php
@@ -4,12 +4,23 @@ $username = "root";
 $password = "";
 $dbname = "bdweb";
 
-
+// Create connection
 $conexao = new mysqli($servername, $username, $password, $dbname);
-
+// Check connection
 if ($conn->connect_error) {
-  echo "Erro";
+  die("Connection failed: " . $conn->connect_error);
 }
-else{
-  echo "Conexão efetuada com sucesso";
+
+$sql = "SELECT id, email FROM usuario";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Email: " . $row["email"]."<br>";
+  }
+} else {
+  echo "0 results";
 }
+$conn->close();
+?>
