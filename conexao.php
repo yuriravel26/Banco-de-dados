66616c35d28a2559d6a6e145114ea1e56ac56619
Yuri <yuri_ravel20@hotmail.com>
2022-05-29 13:47:00 -0300

    fas

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bdweb";
+$dbname = "login";
 
 // Create connection
 $conexao = new mysqli($servername, $username, $password, $dbname);
