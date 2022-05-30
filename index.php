1c38053a354293c3c405c207b9c6a1feb3a8a2a1
LiceHolanda <104989655+LiceHolanda@users.noreply.github.com>
2022-05-30 11:27:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php 
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="widthgit=device-width, initial-scale=1.0">
     <title>BD web</title>
     <link rel="stylesheet" href="index.css">
 </head>
