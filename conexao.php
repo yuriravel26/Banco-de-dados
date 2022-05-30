5dc2f422598318222dfe957827aec1e4db60829f
LiceHolanda <104989655+LiceHolanda@users.noreply.github.com>
2022-05-30 11:26:23 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,25 +2,26 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "bdweb";
+$dbname = "listas_cet";
 
-// Create connection
-$conexao = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conexao->connect_error) {
-  die("Connection failed: " . $conexao->connect_error);
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
 }
 
-$sql = "SELECT id, email FROM usuario";
-$result = $conexao->query($sql);
+$sql = "SELECT * FROM usuario";
+$result = $mysqli->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Email: " . $row["email"]."<br>";
+    echo "id: " . $row["id"]. " -Nome: " . $row["nome"]. " " . "- email: " . $row["email"]."<br>";
   }
 } else {
   echo "0 results";
 }
-$conexao->close();
+$mysqli->close();
+
+
 ?>
